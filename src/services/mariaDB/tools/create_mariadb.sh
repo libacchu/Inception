a267c1a9c29580a669b2c17e3576a6ce9b45d43d
libacchu <libacchu@students.42wolfsburg.de>
2023-02-16 22:39:07 +0100

    update

--- src/services/mariaDB/tools/create_mariadb.sh
+++ src/services/mariaDB/tools/create_mariadb.sh
@@ -5,3 +5,10 @@ mysql_install_db
 /etc/init.d/mysql start
 
 if [ "/var/lib/mysql/" ]
+
+	chown -R mysql:root /var/run/mysqld
+
+	sed -i "s/WORDPRESS_DB_USER/$WORDPRESS_DB_USER/g" /usr/bin/setup.sql
+	sed -i "s/WORDPRESS_DB_PASSWORD/$WORDPRESS_DB_PASSWORD/g" /usr/bin/setup.sql
+	sed -i "s/WORDPRESS_DB_NAME/$WORDPRESS_DB_NAME/g" /usr/bin/setup.sql
+	sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /usr/bin/setup.sql
