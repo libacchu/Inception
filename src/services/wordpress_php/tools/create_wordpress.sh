cb0ca37004a840f6752b38d44f5e450b954009d5
libacchu <libacchu@students.42wolfsburg.de>
2023-02-19 17:56:42 +0100

    update

--- src/services/wordpress_php/tools/create_wordpress.sh
+++ src/services/wordpress_php/tools/create_wordpress.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -f ./wp-config.php]
+if [ ! -f ./wp-config.php ]
 then 
 	echo "WordPress: Downloading..."
 	wget http://wordpress.org/latest.tar.gz
@@ -8,16 +8,16 @@ then
 	rm -rf latest.tar.gz
 
 	echo "WordPress: Configuring..."
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost_here/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	sed -i "s/username_here/$MYSQL_USER/g" ./wordpress/wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" ./wordpress/wp-config-sample.php
+	sed -i "s/localhost_here/$MYSQL_HOSTNAME/g" ./wordpress/wp-config-sample.php
+	sed -i "s/database_here/$MYSQL_DATABASE/g" ./wordpress/wp-config-sample.php
 
-	mv wp-config-sample.php wp-config.php
+	mv ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
 
 	echo "WordPress: Setup completed"
 else
 	echo "WordPress: Is already downloaded and setup"
 fi
 
-exec ./wp-config.php
+exec $@
