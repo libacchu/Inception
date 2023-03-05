748c8b000cbbf4ba5d4305bcf43b22830b0cc3b9
libacchu <libacchu@students.42wolfsburg.de>
2023-03-05 22:22:23 +0100

    update

--- src/services/mariaDB/tools/mariadb.sh
+++ src/services/mariaDB/tools/mariadb.sh
@@ -6,7 +6,7 @@ mysql_install_db
 
 #Check if the database exists
 
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+if [ -d "/var/lib/mysql/$WORDPRESS_DB_DATABASE" ]
 then 
 
 	echo "Database already exists"
@@ -27,14 +27,14 @@ _EOF_
 #Add a root user on 127.0.0.1 to allow remote connexion 
 #Flush privileges allow to your sql tables to be updated automatically when you modify it
 #mysql -uroot launch mysql command line client
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$WORDPRESS_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
 #Create database and user in the database for wordpress
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_DATABASE; GRANT ALL ON $WORDPRESS_DB_DATABASE.* TO '$WORDPRESS_USER_LOGIN'@'%' IDENTIFIED BY '$WORDPRESS_USER_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 
 #Import database in the mysql command line
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+mysql -uroot -p$WORDPRESS_ROOT_PASSWORD $WORDPRESS_DB_DATABASE < /usr/local/bin/wordpress.sql
 
 fi
 
--- src/services/wordpress_php/tools/create_wordpress.sh
+++ src/services/wordpress_php/tools/create_wordpress.sh
@@ -7,24 +7,24 @@ then
 	tar xfz latest.tar.gz
 	rm -rf latest.tar.gz
 
-	echo "WordPress: Configuring..."
-	sed -i "s/username_here/$MYSQL_USER/g" ./wordpress/wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" ./wordpress/wp-config-sample.php
-	sed -i "s/localhost_here/$MYSQL_HOSTNAME/g" ./wordpress/wp-config-sample.php
-	sed -i "s/database_here/$MYSQL_DATABASE/g" ./wordpress/wp-config-sample.php
+	# echo "WordPress: Configuring..."
+	# sed -i "s/username_here/$WORDPRESS_USER_LOGIN/g" ./wordpress/wp-config-sample.php
+	# sed -i "s/password_here/$WORDPRESS_USER_PASSWORD/g" ./wordpress/wp-config-sample.php
+	# sed -i "s/localhost_here/$WORDPRESS_DOMAIN_NAME/g" ./wordpress/wp-config-sample.php
+	# sed -i "s/database_here/$WORDPRESS_DB_DATABASE/g" ./wordpress/wp-config-sample.php
 
-	mv ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
+	# cp ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
 
-	wp core install --allow-root --url=${WORDPRESS_DOMAIN} \
-				--title=${WORDPRESS_TITLE} \
-				--admin_user=${WORDPRESS_ADMIN_LOGIN} \
-				--admin_password=${WORDPRESS_ADMIN_PASSWORD} \
-				--admin_email=${WORDPRESS_ADMIN_EMAIL};
+	# wp core install --allow-root --url=${WORDPRESS_DOMAIN_NAME} \
+	# 			--title=${WORDPRESS_DB_DATABASE} \
+	# 			--admin_user=${WORDPRESS_ADMIN_LOGIN} \
+	# 			--admin_password=${WORDPRESS_ROOT_PASSWORD} \
+	# 			--admin_email=${WORDPRESS_EMAIL};
 
-	wp user create --allow-root ${WORDPRESS_USER_LOGIN} \
-			${WORDPRESS_USER_EMAIL} \
-			--role=contributor \
-			--user_pass=${WORDPRESS_USER_PASSWORD};
+	# wp user create --allow-root ${WORDPRESS_USER_LOGIN} \
+	# 		${WORDPRESS_EMAIL} \
+	# 		--role=contributor \
+	# 		--user_pass=${WORDPRESS_USER_PASSWORD};
 
 	echo "WordPress: Setup completed"
 else
