a0221df295639bf332407b0781f861e5bcd74f97
libacchu <libacchu@students.42wolfsburg.de>
2023-03-09 00:32:43 +0100

    Database: bash script creates user successfully

--- src/services/mariaDB/mariadb.sh
+++ src/services/mariaDB/mariadb.sh
@@ -6,7 +6,7 @@ mysql_install_db
 
 #Check if the database exists
 
-if [ -d "/var/lib/mysql/$WORDPRESS_DB_DATABASE" ]
+if [ -d "/var/lib/mysql/$MARIADB_DATABASE" ]
 then 
 
 	echo "Database already exists"
@@ -15,27 +15,28 @@ else
 # Set root option so that connexion without root password is not possible
 
 mysql_secure_installation << _EOF_
-Y
-4242
-4242
-Y
 n
-Y
-Y
+n
+y
+y
+y
+y
 _EOF_
 
 #Add a root user on 127.0.0.1 to allow remote connexion 
 #Flush privileges allow to your sql tables to be updated automatically when you modify it
 #mysql -uroot launch mysql command line client
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$WORDPRESS_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
 #Create database and user in the database for wordpress
 
-echo "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_DATABASE; GRANT ALL PRIVILEGES ON $WORDPRESS_DB_DATABASE.* TO '$WORDPRESS_USER_LOGIN'@'%' IDENTIFIED BY '$WORDPRESS_USER_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE; GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 
 # #Import database in the mysql command line
-# mysql -uroot -p $WORDPRESS_ROOT_PASSWORD $WORDPRESS_DB_DATABASE < /usr/local/bin/wordpress.sql
+mysql -uroot -p$MARIADB_ROOT_PASSWORD $MARIADB_DATABASE < /usr/local/bin/mariadb.sh
 
 fi
 
 /etc/init.d/mysql stop
+
+exec "$@"
